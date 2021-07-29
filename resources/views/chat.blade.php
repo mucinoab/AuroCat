a438cb5b11684d06fcc8db806f6e5ace5a5e542a
Carlos-Capriel <ccapriel@auronix.com>
2021-07-28 19:12:34 -0500

    Se agrego ejemplo de chat activo y no activo y sus diseños

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -20,15 +20,30 @@ var pusher = new Pusher('{{env("MIX_PUSHER_APP_KEY")}}', {
       <div class="flex-left-head">
         <h2>Chats</h2>
       </div>
+      <div class="chat chat-activo">
+        <div class="chat-icon-cliente">
+        </div>
+        <div class="chat-content">
+          <h2 id="nameUser">Cliente 1</h2>
+          <p id="prev-message">hola mensajito del chat activo prueba previo</p>
+        </div>
+    </div>
       <div class="chat">
           <div class="chat-icon-cliente">
-
           </div>
           <div class="chat-content">
             <h2 id="nameUser">Cliente 1</h2>
-            <p id="prev-message">hola mensajito de prueba previo</p>
+            <p id="prev-message">hola mensajito de prueba  de un chat no activo previo</p>
           </div>
       </div>
+      <div class="chat">
+        <div class="chat-icon-cliente">
+        </div>
+        <div class="chat-content">
+          <h2 id="nameUser">Cliente 1</h2>
+          <p id="prev-message">hola mensajito de prueba  de un chat no activo previo</p>
+        </div>
+    </div>
     </div>
 
     <div class="flex-right">
