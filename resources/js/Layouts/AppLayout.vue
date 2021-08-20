f55225221fc460c29f6a056754181b65cd4ad214
Carlos-Capriel <ccapriel@auronix.com>
2021-08-20 00:05:27 -0500

    Se agrego el color bg-cat-dark-light y se cambio el fondo de las paginas y del header

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -4,8 +4,8 @@
 
         <jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            <nav class="bg-white border-b border-gray-100">
+        <div class="min-h-screen bg-cat-dark">
+            <nav class="bg-cat-dark-light border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
