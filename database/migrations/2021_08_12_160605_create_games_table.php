de21558269362925ea79fbf325e873cbf5858299
Genaro <laureanomg2@gmail.com>
2021-09-14 14:57:04 -0500

    to update

--- database/migrations/2021_08_12_160605_create_games_table.php
+++ database/migrations/2021_08_12_160605_create_games_table.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 class CreateGamesTable extends Migration
 {
     /**
-     * Run the migrations.
+     * Run the migrations
      *
      * @return void
      */
