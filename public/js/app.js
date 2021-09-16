3a5008bb416098169c284dff7e8176e6ed7d502c
Genaro <laureanomg2@gmail.com>
2021-09-16 17:09:08 -0500

    arreglo dde actualización de ganador para cuando ganaba el bot u agente

--- public/js/app.js
+++ public/js/app.js
@@ -21460,12 +21460,11 @@ __webpack_require__.r(__webpack_exports__);
       if (this.option.option == 0) {
         return this.game.state != 2 ? 'En curso' : 'Finalizada';
       } else if (this.option.option == 1) {
-        this.opponent = this.game.opponent ? 'Bot' : 'Agente';
-        return this.opponent;
+        return this.game.opponent ? 'Bot' : 'Agente';
       } else if (this.option.option == 2) {
         if (this.game.winner != undefined) {
           if (this.game.winner == 0) {
-            return this.opponent;
+            return this.game.opponent ? 'Bot' : 'Agente';
           } else if (this.game.winner == 1) {
             return this.name;
           } else if (this.game.winner == 2) {
