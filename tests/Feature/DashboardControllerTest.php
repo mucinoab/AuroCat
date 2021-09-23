a94f62487b0271be824a014fe91aca384551b833
Carlos-Capriel <ccapriel@auronix.com>
2021-09-23 09:50:30 -0500

    Eliminando librerias que no se usan en el test

--- tests/Feature/DashboardControllerTest.php
+++ tests/Feature/DashboardControllerTest.php
@@ -1,9 +1,6 @@
 <?php
 
 namespace Tests\Feature;
-
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class DashboardControllerTest extends TestCase
