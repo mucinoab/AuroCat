141caaea6f5241be0779cf1861bd6f962cca60a4
Carlos-Capriel <ccapriel@auronix.com>
2021-09-23 09:49:02 -0500

    Tests para ruta rates con opcion tgp y tp listo

--- tests/Feature/DashboardControllerTest.php
+++ tests/Feature/DashboardControllerTest.php
@@ -10,12 +10,25 @@ class DashboardControllerTest extends TestCase
 {
     /**
      * Test the route rates with option gwad return OK.
-     *
-     * 
      */
     public function testRouteRatesWithOptionGwad()
     {
         $this->get('rates/gwad')
                 ->assertOk();
     }
+    /**
+     * Test the route rates with option tgp return OK.
+     */
+    public function testRouteRatesWithOptionTgp() {
+        $this->get('rates/tgp')
+                ->assertOk();
+    }
+    /**
+     * Test the route rates with option tp return OK.
+     */
+
+     public function testRouteRatesWithOptionTp(){
+         $this->get('rates/tp')
+                ->assertOk();
+     }
 }
