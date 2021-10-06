79023ea9a2d137253b572747f33a5da5b3e38570
Genaro <laureanomg2@gmail.com>
2021-10-06 10:54:02 -0500

    correción de cambio de estilo a link en vista welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -2,16 +2,16 @@
     <Head title="Welcome" />
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0 dark:bg-cat-hard-5">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            <Link v-if="$page.props.user" :href="route('messenger')" class="text-sm font-medium text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
+            <Link v-if="$page.props.user" :href="route('messenger')" class="text-sm text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
                 Messenger
             </Link>
 
             <template v-else>
-                <Link :href="route('login')" class="text-sm font-medium text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
+                <Link :href="route('login')" class="text-sm text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
                     Iniciar sesión
                 </Link>
 
-                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm font-medium text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
+                <Link v-if="canRegister" :href="route('register')" class="ml-4 text-sm text-gray-700 dark:text-cat-light-secundary-3 underline hover:text-gray-500">
                     Registrarse
                 </Link>
             </template>
