536714e0ba87da2c6b35c33ce257678c46cc08d1
Genaro <laureanomg2@gmail.com>
2021-10-05 23:39:33 -0500

    cambio de tamaño  de letra a la vista welcome

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -30,11 +30,11 @@
                     <div class="p-6">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"></path></svg>
-                            <div class="ml-4 text-2xl leading-7 font-bold"><a href="https://github.com/mucinoab/InternAuro" class="underline text-black dark:text-cat-light-secundary-3">Documentación</a></div>
+                            <div class="ml-4 text-xl leading-7 font-medium"><a href="https://github.com/mucinoab/InternAuro" class="underline text-black dark:text-cat-light-secundary-3">Documentación</a></div>
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-700 dark:text-cat-gray-4 text-base font-medium">
+                            <div class="mt-2 text-gray-700 dark:text-cat-gray-4 text-sm font-normal">
                                 La documentación de nuestro proyecto se encuentra en nuestro repositorio de GitHub.
                             </div>
                         </div>
@@ -44,11 +44,11 @@
                     <div class="p-6  border-gray-200 dark:border-gray-700">
                         <div class="flex items-center">
                             <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500"><path d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg>
-                            <div class="ml-4 text-2xl leading-7 font-bold"><a href="https://t.me/aeolus_help_bot" class="underline text-black dark:text-cat-light-secundary-3">Chabot Telegram</a></div>
+                            <div class="ml-4 text-xl leading-7 font-medium"><a href="https://t.me/aeolus_help_bot" class="underline text-black dark:text-cat-light-secundary-3">Chabot Telegram</a></div>
                         </div>
 
                         <div class="ml-12">
-                            <div class="mt-2 text-gray-700 dark:text-cat-gray-4 text-base font-medium">
+                            <div class="mt-2 text-gray-700 dark:text-cat-gray-4 text-sm font-normal">
                                 Comienza a jugar con nuestro chatbot en Telegram.
                             </div>
                         </div>
